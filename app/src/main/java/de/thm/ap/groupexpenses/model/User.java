1a79d741d3d6293b2210705274ba921cc33c2efa
Hendrik Kegel <hendrik.kegel@mni.thm.de>
2018-12-09 15:42:03 +0100

    added simple friendslist

--- app/src/main/java/de/thm/ap/groupexpenses/model/User.java
+++ app/src/main/java/de/thm/ap/groupexpenses/model/User.java
@@ -1,8 +1,10 @@
 package de.thm.ap.groupexpenses.model;
 
+import android.support.annotation.NonNull;
+
 import java.util.Date;
 
-public class User {
+public class User implements Comparable<User> {
 
     int id;
 
@@ -56,4 +58,15 @@ public class User {
         return this.id == otherUser.getId();
     }
 
+    @NonNull
+    @Override
+    public String toString(){
+        return firstName + " " + lastName;
+    }
+
+    // allows Collections.sort() by first name
+    @Override
+    public int compareTo(User other) {
+        return this.firstName.compareTo(other.firstName);
+    }
 }
