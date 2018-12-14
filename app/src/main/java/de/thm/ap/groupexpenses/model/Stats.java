f3cf5264109b28f9834b329e262536376c5e7c79
Hendrik Kegel <hendrik.kegel@mni.thm.de>
2018-12-14 10:11:35 +0100

    added method

--- app/src/main/java/de/thm/ap/groupexpenses/model/Stats.java
+++ app/src/main/java/de/thm/ap/groupexpenses/model/Stats.java
@@ -1,10 +1,45 @@
 package de.thm.ap.groupexpenses.model;
 
+import android.support.annotation.NonNull;
+import android.util.ArrayMap;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import de.thm.ap.groupexpenses.App;
 
 public class Stats {
 
     private static final String TAG = Stats.class.getName();
+
+    public Map<Event, Float> calculateAll(List<Event> events){
+        if (App.CurrentUser == null)
+            throw new IllegalStateException("User not specified!");
+        return calculateAll(App.CurrentUser, events);
+    }
+
+    public Map<Event, Float> calculateAll(User appuser, List<Event> events){
+        Map<Event, Float> result = new HashMap<>();
+        for (Event event: events){
+            result.put(event, getEventBalance(appuser, event));
+        }
+        return result;
+    }
+
+    public float getBalance(@NonNull List<Event> events){
+        if (App.CurrentUser == null)
+            throw new IllegalStateException("User not specified!");
+        return getBalance(App.CurrentUser, events);
+    }
+
+    public float getBalance(@NonNull User creator, @NonNull List<Event> events){
+        float sum = 0.00f;
+        for(Event e: events) sum += getEventBalance(creator, e);
+        return sum;
+    }
+
+
 
     public float getEventBalance(Event e){
         if (App.CurrentUser == null){
