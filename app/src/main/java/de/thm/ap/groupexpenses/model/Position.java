0651556cd8301a2f0894afd168161ff45ff05cf5
Hendrik Kegel <hendrik.kegel@mni.thm.de>
2019-01-07 13:40:22 +0100

    added stuff to position

--- app/src/main/java/de/thm/ap/groupexpenses/model/Position.java
+++ app/src/main/java/de/thm/ap/groupexpenses/model/Position.java
@@ -3,8 +3,8 @@ package de.thm.ap.groupexpenses.model;
 import android.support.annotation.NonNull;
 
 import java.io.Serializable;
-import java.util.Calendar;
-import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class Position implements Serializable {
@@ -15,71 +15,112 @@ public class Position implements Serializable {
     private String info;
     private Long date;
     private String creatorId;
+    private List<String> peopleThatDontHaveToPay;
 
+    //region constructor
     public Position(){}
-
     public Position(String creatorId, String topic, Float value){
         this.creatorId = creatorId;
         this.topic = new HistoryValue<>(topic);
         this.value = new HistoryValue<>(value);
+        this.peopleThatDontHaveToPay = new ArrayList<>();
+        this.peopleThatDontHaveToPay.add(creatorId);
     }
-
     public Position(int positionId, String creatorId, String topic, Float value){
         this.pid = positionId;
         this.creatorId = creatorId;
         this.topic = new HistoryValue<>(topic);
         this.value = new HistoryValue<>(value);
+        this.peopleThatDontHaveToPay = new ArrayList<>();
+        this.peopleThatDontHaveToPay.add(creatorId);
     }
+    public Position(int positionId, String creatorId, String topic, Float value, List<String> excludedPeople){
+        this.pid = positionId;
+        this.creatorId = creatorId;
+        this.topic = new HistoryValue<>(topic);
+        this.value = new HistoryValue<>(value);
+        this.peopleThatDontHaveToPay = new ArrayList<>();
+        this.peopleThatDontHaveToPay.add(creatorId);
+        this.peopleThatDontHaveToPay.addAll(excludedPeople);
+    }
+    //endregion
 
+    //region getter/setter
     public int getPid() {
         return pid;
     }
-
     public void setPid(int pid) {
         this.pid = pid;
     }
-
     public String getTopic() {
         return topic.get();
     }
-
     public void setTopic(String topic) {
         this.topic.set(topic);
     }
-
     public Float getValue() {
         return value.get();
     }
-
     public void setValue(Float value) {
         this.value.set(value);
     }
-
     public String getInfo() {
         return info;
     }
-
     public void setInfo(String info) {
         this.info = info;
     }
-
     public Long getDate() {
         return date;
     }
-
     public void setDate(Long date) {
         this.date = date;
     }
-
     public String getCreatorId() {
         return creatorId;
     }
-
     public Map<Long, String> getTopicHistory() {
         return topic.getHistory();
     }
     public Map<Long, Float> getValueHistory() {
         return value.getHistory();
+    }
+    //endregion
+
+    //region public methods
+
+    public void removeDebtor(String debtorId){
+        this.peopleThatDontHaveToPay.add(debtorId);
+    }
+    public boolean hasDebts(String debtorId) {
+        return !peopleThatDontHaveToPay.contains(debtorId);
+    }
+
+    /**
+     * Returns debts of single person. The debt is owed to the creator.
+     * @param userId
+     * @param userCount amount of users that share the costs including the creator
+     * @return
+     */
+    public float getDebtOfUser(String userId, int userCount) {
+        if (!hasDebts(userId)) return 0.f;
+        return (1.f/userCount)*value.get();
+    }
+
+    public float getCredit(String creditorId, List<String> involvedPeople) {
+        if (!isCreator(creditorId)) return 0.f;
+
+        float credit = 0.f;
+
+        for(String userId : involvedPeople){
+            credit += getDebtOfUser(userId, involvedPeople.size());
+        }
+
+        return credit;
+    }
+
+    public boolean isClosable(List<String> involvedPeople) {
+        return peopleThatDontHaveToPay.containsAll(involvedPeople);
     }
 
     public float getFactorizedValue(float factor){
@@ -90,5 +131,10 @@ public class Position implements Serializable {
     @Override
     public String toString() {
         return topic + ": " + value.toString() + " by " + creatorId;
+    }
+    //endregion
+
+    private boolean isCreator(String userId){
+        return this.creatorId.equals(userId);
     }
 }
--- app/src/test/java/de/thm/ap/groupexpenses/PositionTest.java
+++ app/src/test/java/de/thm/ap/groupexpenses/PositionTest.java
@@ -1,17 +1,36 @@
 package de.thm.ap.groupexpenses;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import de.thm.ap.groupexpenses.model.Position;
 
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class PositionTest {
 
-    Position position = new Position(1,"id1", "Bier", 30f);
+    private Position position = new Position(1,"id1", "Bier", 30f);
+    private List<String> involvedPeople;
+
+    @Before
+    public void setup(){
+        involvedPeople = new ArrayList<>();
+        involvedPeople.add("id1");  // the creator is always involved
+        involvedPeople.add("John");
+        involvedPeople.add("Tina");
+        involvedPeople.add("Tom");
+        involvedPeople.add("Jan");
+        involvedPeople.add("Dan");
+    }
 
     @Test
     public void positionTest(){
@@ -33,5 +52,73 @@ public class PositionTest {
         int BREAKPOINT = 0;
     }
 
+    @Test
+    public void getDebtTest(){
 
+        int randomUserCount = getRandomInt();
+        float debt = position.getDebtOfUser("userThatIsNotExcludedFromPaymentForSure", randomUserCount);
+
+        //value = 30, shared by 5 users = 6€ per user
+        assertEquals(debt, 30f/randomUserCount, 0.01);
+
+        debt = position.getDebtOfUser("id1", getRandomInt());
+
+        // the user is the creator - he does not owe himself: debt=0f
+        assertEquals(debt,0f, 0.01);
+
+        position.removeDebtor("John");
+        debt = position.getDebtOfUser("John", getRandomInt());
+
+        // John has payed - therefore he has no depts in here
+        assertEquals(debt, 0f, 0.01);
+    }
+
+    @Test
+    public void getCreditTest(){
+
+        float credit = position.getCredit(getRandomString(10), involvedPeople);
+
+        // credit is 0 cause the creditor doesnt match the creator, therefore they cant have a credit
+        assertEquals(credit, 0f, 0.01);
+
+        credit = position.getCredit("id1", involvedPeople);
+
+        //value=30, 6 ppl involved, 5 have to pay: 30/6*5 = 25
+        assertEquals(credit, 25, 0.01);
+
+        position.removeDebtor("John"); // john payed
+
+        credit = position.getCredit("id1", involvedPeople);
+
+        //value=30, 6 ppl involved, 4 have to pay: 30/6*4 = 20
+        assertEquals(credit, 20, 0.01);
+    }
+
+    @Test
+    public void isClosableTest(){
+
+        assertFalse(position.isClosable(involvedPeople));
+
+        position.removeDebtor("John");
+        position.removeDebtor("Tina");
+        position.removeDebtor("Tom");
+        position.removeDebtor("Jan");
+
+        assertFalse(position.isClosable(involvedPeople));
+
+        position.removeDebtor("Dan");
+
+        assertTrue(position.isClosable(involvedPeople));
+    }
+
+    //region helper
+    private int getRandomInt(){
+        return (int)(Math.random()*100);
+    }
+    private String getRandomString(int size) {
+        byte[] array = new byte[size];
+        new Random().nextBytes(array);
+        return new String(array, Charset.forName("UTF-8"));
+    }
+    //endregion
 }
