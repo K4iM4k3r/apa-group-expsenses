e661a690b1ac5e0931a936413fd58c73fb63d16c
kai <kai.schaefer@mni.thm.de>
2019-03-02 19:09:33 +0100

    fix server checkout process

--- BraintreePayments/checkout.php
+++ BraintreePayments/checkout.php
@@ -1,15 +1,24 @@
 <?php
 require_once ("include/braintree_init.php");
-require_once 'vendor/braintree/braintree_php/lib/Braintree.php';
+require_once ("vendor/braintree/braintree_php/lib/Braintree.php");
 
 $nonce = $_POST['nonce'];
 $amount = $_POST['amount'];
-$result = Braintree_Transaction::sale([
+
+/*$result = Braintree_Transaction::sale([
   'amount' => $amount,
   'paymentMethodNonce' => $nonce,
   'options' => [
     'submitForSettlement' => True
   ]
+]);*/
+$result = $gateway->transaction()->sale([
+    'amount' => $amount,
+    'paymentMethodNonce' => $nonce,
+    'options' => [
+        'submitForSettlement' => true
+    ]
 ]);
+
 echo $result;
 ?>
